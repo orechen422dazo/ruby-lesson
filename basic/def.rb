5a38b948513b84db65ecf2c111c07f2e6a79560b
orechen422dazo <jboy422hobby@gmail.com>
2024-09-21 16:33:05 +0900

    added: class

--- basic/def.rb
+++ basic/def.rb
@@ -1,15 +1,15 @@
 # メソッドの定義
 def greet(name)
-    "Hello, #{name}!"
-  end
-  
-  # メソッドの呼び出し
-  puts greet("Bob")
-  
-  # 引数にデフォルト値を設定
-  def power(base, exponent = 2)
-    base ** exponent
-  end
-  
-  puts power(3)    # 3の2乗
-  puts power(2, 3) # 2の3乗
+  "Hello, #{name}!"
+end
+
+# メソッドの呼び出し
+puts greet('Bob')
+
+# 引数にデフォルト値を設定
+def power(base, exponent = 2)
+  base ** exponent
+end
+
+puts power(3) # 3の2乗
+puts power(2, 3) # 2の3乗
---
+++ basic3/def.rb
@@ -0,0 +1,6 @@
+def calc(a, b)
+  a + b
+end
+
+c = calc(1, 2)
+puts c
